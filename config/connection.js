83ff7e1e61db7216ba2337688081041c42e60005
ashleyerffmeyer <erffmea@gmail.com>
2019-09-12 18:35:27 -0500

    updated connection.js file

--- config/connection.js
+++ config/connection.js
@@ -10,6 +10,17 @@ var connection = mysql.createConnection({
     database: "burgers_db"
 });
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "19821982Ame$",
+        database: "burgers_db"
+    });
+};
+
 // Making connection
 connection.connect(function (err) {
     if (err) {
